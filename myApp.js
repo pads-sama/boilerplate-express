786e0cd6c62381707028b6ecbf5a23ed342e81e9
Juan Carlos Padillo <juancarlospadillo23@gmail.com>
2024-04-14 23:07:11 +0800

    finished course

--- myApp.js
+++ myApp.js
@@ -1,5 +1,63 @@
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const { populate } = require('dotenv');
 let express = require('express');
 let app = express();
+let path = require('path');
+
+console.log("Hello World");
+
+//serve public folders
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
+//logger
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
+
+//body parser extended = false
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//display the index.html from views folder
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+//return json data
+app.get('/json', (req, res) => {
+    let message = "Hello json";
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        message = message.toUpperCase();
+    }
+    res.json({ "message": message });
+});
+
+//return the time of request
+const currentTime = () => new Date().toString();
+
+app.get('/now', (req, res, next) => {
+    req.time = currentTime();
+    next();
+}, (req, res) => {
+    res.json({ "time": req.time });
+});
+
+
+//display the echod word
+app.get('/:word/echo', (req, res) => {
+    const echoWord = req.params.word;
+    res.json({ "echo": echoWord });
+});
+
+
+//req.query
+app.route('/name').get((req, res) => {
+    res.json({ "name": `${req.query.first} ${req.query.last}` });
+}).post((req, res) => {
+    //return the data from the form
+    res.json({ "name": `${req.body.first} ${req.body.last}` });
+});
 
 
 
@@ -33,7 +91,4 @@ let app = express();
 
 
 
-
-
-
- module.exports = app;
+module.exports = app;
